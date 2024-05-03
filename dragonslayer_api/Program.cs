46af9fc0c9ad837e4b5260e828d5375c0dc93814
Andrew Haarsager <athaarsager@gmail.com>
2024-05-03 11:04:25 -0500

    Create objects representing database tables

--- dragonslayer_api/Program.cs
+++ dragonslayer_api/Program.cs
@@ -1,6 +1,50 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+// api goes here
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+// define classes here
+public class GameData
+{
+    public class CharacterClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Denial_Text { get; set; }
+    }
+
+    public class Stat
+    {
+        public int Id { get; set; }
+        public int Class_Id { get; set; }
+        public int HP { get; set; }
+        public int Mana { get; set; }
+        public int Defense { get; set; }
+        public int Attack { get; set; }
+    }
+
+    public class Attack
+    {
+        public int Id { get; set; }
+        public int Class_Id { get; set; }
+        public string Name { get; set; }
+        public int Mana_Cost { get; set; }
+        public int Power { get; set; }
+        public string Description { get; set; }
+        public string Attack_Text { get; set; }
+        // Referencing columns in the attack_effects table
+        public string Effect_Target_Character { get; set; }
+        public string Effect_Target_Stat { get; set; }
+        public int Effect_Multiplier { get; set; }
+        public string Other_Outcome { get; set; }
+
+    }
+}
