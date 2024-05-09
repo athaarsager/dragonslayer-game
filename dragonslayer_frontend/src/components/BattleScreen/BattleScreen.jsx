b3e15a32fe784f491c505290526b810c834ee637
Andrew Haarsager <athaarsager@gmail.com>
2024-05-09 10:39:13 -0500

    Adjust positioning, add boxes for displaying text

--- dragonslayer_frontend/src/components/BattleScreen/BattleScreen.jsx
+++ dragonslayer_frontend/src/components/BattleScreen/BattleScreen.jsx
@@ -8,11 +8,17 @@ function BattleScreen() {
     return (
         <>
             <h2>A dragon draws near!</h2>
-            <div id="dragon-display">
-                {/* Credit for dragon image: Image by Artie Blur from Pixabay 
+            {/* Credit for dragon image: Image by Artie Blur from Pixabay 
                 Granted, it is AI generated, so do I need to credit him? Probablys still should...*/}
+            <div id="dragon-display">
                 <img src="/public/images/dragon.jpg"
                     alt="A dark blue dragon whose tail and wings exude flames as it sets a forest on fire in the night" />
+            </div>
+            <div id="battle-text" class="text-box">
+                <p>Battle Text Here</p>
+            </div>
+            <div id="battle-menu" class="text-box">
+                <p>Action Menu Here</p>
             </div>
         </>
     )
