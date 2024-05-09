cceb9c42d77c5e19b1940fc2b8b445d3334b05cc
Andrew Haarsager <athaarsager@gmail.com>
2024-05-09 10:27:15 -0500

    Center dragon on screen

--- dragonslayer_frontend/src/components/BattleScreen/BattleScreen.jsx
+++ dragonslayer_frontend/src/components/BattleScreen/BattleScreen.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import axios from "axios";
+import "./BattleScreen.css";
 function BattleScreen() {
 
     // putting axios calls here for now. Will very likely need to move them to a different component later
 
     return (
         <>
-        <h2>A dragon draws near!</h2>
-        {/* Credit for dragon image: Image by Artie Blur from Pixabay 
-        Granted, it is AI generated, so do I need to credit him? Probablys still should...*/}
-        <img src="/public/images/dragon.jpg" 
-        alt="A dark blue dragon whose tail and wings exude flames as it sets a forest on fire in the night"/>
+            <h2>A dragon draws near!</h2>
+            <div id="dragon-display">
+                {/* Credit for dragon image: Image by Artie Blur from Pixabay 
+                Granted, it is AI generated, so do I need to credit him? Probablys still should...*/}
+                <img src="/public/images/dragon.jpg"
+                    alt="A dark blue dragon whose tail and wings exude flames as it sets a forest on fire in the night" />
+            </div>
         </>
     )
 }
