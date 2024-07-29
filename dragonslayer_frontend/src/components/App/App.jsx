e450c281ae12da60f37db8bb65a817f5edfc2440
Andrew Haarsager <athaarsager@gmail.com>
2024-07-29 18:57:41 -0500

    Create and import page for Prologue. Create variable for playerName

--- dragonslayer_frontend/src/components/App/App.jsx
+++ dragonslayer_frontend/src/components/App/App.jsx
@@ -1,17 +1,22 @@
-import TitleScreen from '../TitleScreen/TitleScreen.jsx'
-import BattleScreen from '../BattleScreen/BattleScreen.jsx'
-import { useState } from 'react'
+import TitleScreen from '../TitleScreen/TitleScreen.jsx';
+import BattleScreen from '../BattleScreen/BattleScreen.jsx';
+import ProloguePage from '../ProloguePage/ProloguePage.jsx';
+import { useState } from 'react';
 
 export default function App() {
 
+    const [playerName, setPlayerName] = useState("");
+
     const [onTitleScreen, setOnTitleScreen] = useState(true);
     const [onBattleScreen, setOnBattleScreen] = useState(false);
+    const [onProloguePage, setOnProloguePage] = useState(false);
 
     return (
         <>
             {onTitleScreen &&
                 <TitleScreen
-                     setOnTitleScreen={setOnTitleScreen} />
+                     setOnTitleScreen={setOnTitleScreen} 
+                     setOnProloguePage={setOnProloguePage}/>
             }
             {onBattleScreen &&
                 <BattleScreen />
---
+++ dragonslayer_frontend/src/components/ProloguePage/ProloguePage.jsx
@@ -0,0 +1,8 @@
+function ProloguePage() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default ProloguePage;
