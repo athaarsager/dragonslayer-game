52609e3848216518bee558586615088a176a11d0
Andrew Haarsager <athaarsager@gmail.com>
2024-07-29 14:43:34 -0500

    Add image to title screen

--- dragonslayer_frontend/src/components/BattleScreen/BattleScreen.jsx
+++ dragonslayer_frontend/src/components/BattleScreen/BattleScreen.jsx
@@ -489,7 +489,7 @@ function BattleScreen() {
                     {/* Credit for dragon image: Image by Artie Blur from Pixabay 
                     Granted, it is AI generated, so do I need to credit him? Probablys still should...*/}
                     <div id="dragon-display">
-                        <img src="/public/images/dragon.jpg"
+                        <img id="dragon-image" src="/public/images/dragon.jpg"
                             alt="A dark blue dragon whose tail and wings exude flames as it sets a forest on fire in the night" />
                     </div>
                 </> : badEndingText.length > 0 && /* This component will only render if badEndingText is populated */
--- dragonslayer_frontend/src/components/TitleScreen/TitleScreen.jsx
+++ dragonslayer_frontend/src/components/TitleScreen/TitleScreen.jsx
@@ -18,6 +18,10 @@ function TitleScreen({ setOnTitleScreen }) {
                 <p id="left-title-text">Dragon</p>
                 <p id="right-title-text">Slayer</p>
             </div>
+            {/* Image by <a href="https://pixabay.com/users/clker-free-vector-images-3736/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=33979">Clker-Free-Vector-Images</a>
+             from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=33979">
+             Pixabay</a> */}
+            <img id="swords-image" src="/public/images/swordsAndShield.png" alt="Two swords crossed over a shield"/>
             <p className="start-text">Press Spacebar or Enter to Begin</p>
         </>
     )
