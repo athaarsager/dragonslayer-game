4ae5baecb5601e35f25a49d5a380aaebd7abda1c
Andrew Haarsager <athaarsager@gmail.com>
2024-07-29 16:03:48 -0500

    Start text animation behaves correctly on keyPress

--- dragonslayer_frontend/src/components/TitleScreen/TitleScreen.jsx
+++ dragonslayer_frontend/src/components/TitleScreen/TitleScreen.jsx
@@ -34,19 +34,30 @@ function TitleScreen({ setOnTitleScreen }) {
 
         const handleKeyPress = (e) => {
             if (e.key === " " || e.key === "Enter")
-            if (animationRef.current) {
-                // progress lets me set where I would like the animation to skip to
-                animationRef.current.progress(1);
-                setDisplay("");
-            }
+                if (animationRef.current) {
+                    // progress lets me set where I would like the animation to skip to
+                    animationRef.current.progress(1);
+                    setDisplay("");
+                }
+            gsap.timeline()
+                .to("#start-text", {
+                    opacity: 0,
+                    duration: 0.15,
+                    repeat: 5,
+                    // yoyo: true,
+                    ease: "power1.inOut"
+                })
+                .to("#start-text", {
+                    opacity: 1,
+                    duration: 0.15,
+                    ease: "power1.inOut"
+                });
+
+            document.removeEventListener("keydown", handleKeyPress);
         }
 
         document.addEventListener("keydown", handleKeyPress);
 
-        // cleanup event listeners when component unmounts
-        return () => {
-            document.removeEventListener("keydown", handleKeyPress);
-        }
 
     }, []);
 
