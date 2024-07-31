508556e01a4ed46c526c0b7d23bba9aec24d07e9
Andrew Haarsager <athaarsager@gmail.com>
2024-07-30 19:01:27 -0500

    Bring over logic for progressing the narration

--- dragonslayer_frontend/src/components/ProloguePage/ProloguePage.jsx
+++ dragonslayer_frontend/src/components/ProloguePage/ProloguePage.jsx
@@ -6,6 +6,33 @@ function ProloguePage({ openingText, prologueText, playerName, setPlayerName }) 
 
     const [narrationText, setNarrationText] = useState(openingText.length > 0 ? openingText[0].textContent : "");
 
+    let resolveKeyPress =  null;
+
+    // Re-using some functionality I wrote for the other narration page below.
+    // There's probably a more efficient way for me to do this aside from copy-pasting. Oh well
+    function appendText(newText) {
+        setNarrationText(currentText => currentText + newText);
+    }
+
+    function progressText() {
+        return new Promise((resolve) => {
+            // This is what changes resolveKeyPress from null to truthy
+            // when it is resolved in the below function, it resolves it here as well
+            // resolve is a function that is assigned to a variable here so it can be accessed
+            // outside of the promise
+            resolveKeyPress = resolve;
+        });
+    }
+
+    // This function is added via event listener and executes when a key is pressed
+    function resolveUserInput(e) {
+        // resolveKeyPress is set to a value whenever progressRound is called
+        if (resolveKeyPress && (e.key === " " || e.key === "Enter")) {
+            resolveKeyPress(); // Resolve the Promise when the desired key is pressed thanks to the resolve function stored in this variable
+            resolveKeyPress = null; // Reset the resolveKeyPress variable
+        }
+    }
+
     return (
         <div id="narration-container">
             <p className="prologue-text">{narrationText}</p>
