bc4f262de76841296ffd57ab56d6ab14b37c3d17
Andrew Haarsager <athaarsager@gmail.com>
2024-08-08 16:22:13 -0500

    remove unnecessary package.json files

--- dragonslayer_api/obj/Debug/net8.0/dragonslayer_api.AssemblyInfo.cs
+++ dragonslayer_api/obj/Debug/net8.0/dragonslayer_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dragonslayer_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+095af585dc849663849ddcecfbbfbd9cda867b04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73edbffb67caad65445b3adb2e84a3b51f5648c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("dragonslayer_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dragonslayer_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
